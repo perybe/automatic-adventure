98918dfee8da957e01c5d76c8308a286e4473604
REBECA CRISTINA LIMA GOMES <16104083@l1801117.portoalegre.pucrsnet.br>
2016-05-16 11:07:22 -0300

    freq

---
+++ freq.c
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#define SIZE 10 /*tamanho do array*/
+
+int main()
+{
+	
+
+	int s[ SIZE ]; /* array s tem SIZE elementos*/
+	int i; /*contador do s*/
+	int total = 0;
+	FILE *arquivo;
+
+	arquivo = fopen ("freq.txt", "r");
+
+	for( i = 0; i < SIZE; i++)
+	{
+		s[i] = 0 ;
+	}
+
+	s['A']++;
+	s['B']++;
+	s['C']++;
+
+	fclose(arquivo);
+	
+	for( i = 0; i <SIZE; i++)
+	{
+		total += s[i];
+	}
+	
+	printf("%8s %13s\n", "ELEMENTO", "VALOR");
+
+	for(i = 0; i < SIZE; i++)
+	{
+		if (s[i] != 0)
+		{
+		printf("%8d %4c %5x %13d\n", i, i, i, s[i]);
+		}
+	}
+
+	printf( "%8s%13d\n", "TOTAL", total);
+
+	return EXIT_SUCCESS;
+}
+
+
---
+++ lerbyte1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	FILE *arquivo;
+	int c;
+
+	arquivo = fopen("lerbyte1", "r");
+	int i;
+
+	for( i = 0; i < 10; i++);
+		c = getc(arquivo);
+		printf("%i %c %x\n", c ,c, c);
+
+	}
+	fclose(arquivo);	
+
+	return 0;
+}
